b2328f34275a0da1c233b80ef829d31f8eb4a53d
Kent C. Dodds <me+github@kentcdodds.com>
2020-08-26 16:54:22 -0600

    initial stuff working

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,79 @@
-async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({success: true}),
+import serverless from 'serverless-http'
+import polka from 'polka'
+import send from '@polka/send-type'
+import got from 'got'
+
+const app = polka()
+
+function handleError(middleware) {
+  return (req, res, next) =>
+    middleware(req, res, next).catch(error =>
+      send(res, error.statusCode ?? 500, {
+        message: error.message,
+        body: error.body,
+        statusCode: error.statusCode ?? 500,
+      }),
+    )
+}
+
+function safeParse(string) {
+  try {
+    return JSON.parse(string)
+  } catch (error) {
+    return string
   }
 }
+
+function handleGotError(error) {
+  error.statusCode = error.response.statusCode
+  try {
+    const body = safeParse(error.response.body)
+    error.body = body
+    error.message = JSON.stringify(body, null, 2) || error.message
+  } catch {
+    // ignore
+  }
+  return error
+}
+
+app.get(
+  '/.netlify/functions/api/ping',
+  handleError(async (req, res) => {
+    send(res, 200, 'pong')
+  }),
+)
+
+app.get(
+  '/.netlify/functions/api/resolve-discord-code',
+  handleError(async (req, res) => {
+    const authResult = await got
+      .post('https://discordapp.com/api/oauth2/token', {
+        form: {
+          client_id: process.env.REACT_APP_DISCORD_CLIENT_ID,
+          client_secret: process.env.REACT_APP_DISCORD_CLIENT_SECRET,
+          grant_type: 'authorization_code',
+          redirect_uri: 'http://localhost:8888',
+          code: req.query.code,
+          scope: 'identify',
+        },
+        hooks: {beforeError: [handleGotError]},
+      })
+      .json()
+
+    const userInfo = await got
+      .get('https://discordapp.com/api/users/@me', {
+        responseType: 'json',
+        headers: {
+          authorization: `${authResult.token_type} ${authResult.access_token}`,
+        },
+        hooks: {beforeError: [handleGotError]},
+      })
+      .json()
+
+    send(res, 200, userInfo)
+  }),
+)
+
+const handler = serverless(app)
 
 export {handler}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ky from 'ky'
+
+const api = ky.create({
+  prefixUrl: `${process.env.REACT_APP_NETLIFY_FUNCTIONS_URL}/api`,
+  retry: 0,
+})
+
+async function getUserInfo(code) {
+  const result = await api
+    .get('resolve-discord-code', {searchParams: {code}})
+    .json()
+  console.log({result})
+  return result
+}
 
 function App() {
-  return <div>Hello world</div>
+  const [userInfo, setUserInfo] = React.useState(null)
+  React.useEffect(() => {
+    const fragment = new URLSearchParams(window.location.search.slice(1))
+
+    if (fragment.has('code')) {
+      console.log('has code')
+      getUserInfo(fragment.get('code'))
+        .then(response => {
+          setUserInfo(response)
+        })
+        .catch(console.error)
+    }
+  }, [])
+  return (
+    <>
+      <div>Hello world</div>
+      <a href="https://discord.com/api/oauth2/authorize?client_id=738096608440483870&redirect_uri=http%3A%2F%2Flocalhost%3A8888&response_type=code&scope=identify">
+        Get info
+      </a>
+      {userInfo ? <pre>{JSON.stringify(userInfo, null, 2)}</pre> : null}
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('⚛'))
